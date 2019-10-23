733a98e2c30ed4d048d7932dbaaba07019e4facc
Aleksey Slutskiy <aslutskiy@apstra.com>
2019-10-23 12:42:08 +0400

    First experiments with libjit

--- libjitparser.c
+++ libjitparser.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <jit/jit.h>
 
+
 int main(int argc, char **argv)
 {
 	jit_context_t context;
-	jit_type_t params[3];
+	jit_type_t params[1];
 	jit_type_t signature;
 	jit_function_t function;
-	jit_value_t x, y, z;
-	jit_value_t temp1, temp2;
-	jit_int arg1, arg2, arg3;
-	void *args[3];
+	jit_value_t x;
+	//jit_value_t res[10];
+	jit_int arg1;
+	void *args[1];
 	jit_int result;
 
 	/* Create a context to hold the JIT's primary state */
@@ -21,21 +22,20 @@ int main(int argc, char **argv)
 
 	/* Build the function signature */
 	params[0] = jit_type_int;
-	params[1] = jit_type_int;
-	params[2] = jit_type_int;
 	signature = jit_type_create_signature
-		(jit_abi_cdecl, jit_type_int, params, 3, 1);
+		(jit_abi_cdecl, jit_type_int, params, 1, 1);
 
 	/* Create the function object */
 	function = jit_function_create(context, signature);
 
 	/* Construct the function body */
 	x = jit_value_get_param(function, 0);
-	y = jit_value_get_param(function, 1);
-	z = jit_value_get_param(function, 2);
-	temp1 = jit_insn_mul(function, x, y);
-	temp2 = jit_insn_add(function, temp1, z);
-	jit_insn_return(function, temp2);
+    jit_value_t size = jit_value_create_nint_constant(function, jit_type_int, 100);
+    jit_value_t res = jit_insn_alloca(function, size);
+    jit_value_t i = jit_value_create(function, jit_type_int);
+    jit_insn_store_elem(function, res, i, x);
+    
+    jit_insn_return(function, i);
 
 	/* Compile the function */
 	jit_function_compile(function);
@@ -45,11 +45,7 @@ int main(int argc, char **argv)
 
 	/* Execute the function and print the result */
 	arg1 = 3;
-	arg2 = 5;
-	arg3 = 2;
 	args[0] = &arg1;
-	args[1] = &arg2;
-	args[2] = &arg3;
 	jit_function_apply(function, args, &result);
 	printf("mul_add(3, 5, 2) = %d\n", (int)result);
 
